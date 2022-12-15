6ec66a74f24a470f2b72884fab18c50498e4b93e
okestro-jelee <je.lee@okestro.com>
2022-12-15 10:58:20 +0900

    anomaly detection commit

--- anomaly_detection/log_anomaly_train.py
+++ anomaly_detection/log_anomaly_train.py
@@ -115,3 +115,87 @@ class Log_Anomaly_Model():
         data = self.es.search(index= 'sym-log-syslog*',body = body, track_total_hits= True, size=0)
         host_list = [hash_map['key'] for hash_map in data['aggregations']['keys']['buckets']]
         return host_list
+
+    @timed
+    def load_host_data(self, hostname, gte=None, lte=None):
+        if gte is None:
+            gte = self.gte_date
+        if lte is None:
+            lte = self.now
+        os.makedirs('{project_path}/host_log'.format(project_path=self.project_path), exist_ok=True)
+        program_filter_list = ["logstash"]
+
+        index = "sym-log-syslog"
+        body = {
+            "query": {
+                "bool": {
+                    "must": [
+                        {
+                            "range": {
+                                "@timestamp": {
+                                    "gte": gte.isoformat(sep="T"),
+                                    "lte": lte.isoformat(sep="T")
+                                }
+                            }
+                        },
+                            {'terms' : {
+                                'host.hostname.keyword' : [hostname]
+                            }}
+                    ],
+                    "must_not": [
+                        {
+                            "terms": {
+                                "program.keyword": program_filter_list
+                            }
+                        }
+                    ]
+                }
+            }
+        }
+        res = self.response(self.es, index, body)
+        res_df = pd.DataFrame()
+
+        syslog_fields = ["@timestamp", "@version", "agent.type",
+                         "host.hostname", "host.id",
+                         "input.type", "log.file.path", "log.offset", "message", "pid", "program"]
+
+        count = 0
+        now = time.time()
+        start = now
+        with open('{project_path}/host_log/{hostname}.csv'.format(hostname = hostname,
+                                                                  project_path=self.project_path), 'w') as file:
+            writer = csv.DictWriter(file, fieldnames=syslog_fields)
+            header = writer.writeheader()
+            for row in res:
+                result_dict = {}
+                dict_list = []
+                org_row = row['_source']
+                for item in syslog_fields:
+                    flag = True
+                    row = org_row
+                    for i in item.split("."):
+                        try:
+                            row = row[i]
+                        except KeyError:
+                            flag = False
+                    if flag is True:
+                        result_dict[item] = row
+                    else:
+                        pass
+                # df_dict = pd.DataFrame([result_dict])
+                # res_df = pd.concat([res_df, df_dict], ignore_index=True)
+                writer.writerow(result_dict)
+                count +=1
+                if count%100000 == 0:
+                    standardLog.sending_log('warning').warning(\
+                        f'AnoamlyDetection elapsed time for 100000 rows : {time.time() - now}')
+                    now = time.time()
+                if count%1000000 == 0:
+                    standardLog.sending_log('warning').warning(\
+                        f'AnoamlyDetection host has more than maximum log counts model will be trained with {self.max_log_counts} logs')
+                    break
+            standardLog.sending_log('success').info(f'AnoamlyDetection total {count} rows loaded during {time.time() - start} secs')
+        res_df = pd.read_csv('{project_path}/host_log/{hostname}.csv'.format(project_path=self.project_path,
+                                                                             hostname = hostname))
+        res_df.fillna(0, inplace=True)
+        return res_df
