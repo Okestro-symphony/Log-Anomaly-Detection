46fb8d0d81979cab6bf60c2f617f18945faed171
okestro-jelee <je.lee@okestro.com>
2022-12-21 16:07:44 +0900

    log-anomaly-detection commit

--- base/base_model.py
+++ base/base_model.py
@@ -67,3 +67,76 @@ class ForcastBasedModel(nn.Module):
             session_df['labels'] = (session_df[f"preds"] > thre).astype(int)
 
             return session_df[["preds","labels"]]
+
+        
+    def test(self, test_loader):
+        self.eval() 
+        with torch.no_grad():
+            y_pred = []
+            store_dict = defaultdict(list)
+            infer_start = time.time()
+            for batch_input in test_loader:
+                return_dict = self.forward(self.__input2device(batch_input))
+                y_pred = return_dict["y_pred"]
+                store_dict["label"].extend(
+                    tensor2flatten_arr(batch_input["label"])
+                )
+                store_dict["preds"].extend(tensor2flatten_arr(y_pred))
+            infer_end = time.time()
+            logging.info("Finish inference [{:.2f}s]".format(infer_end - infer_start))
+            self.time_tracker["test"] = infer_end - infer_start
+
+            store_df = pd.DataFrame(store_dict)
+
+            use_cols = ["label", "preds"]
+            session_df = (
+                store_df[use_cols]
+            )
+            assert (self.anomaly_ratio is not None), "anomaly_ratio should be specified for autoencoder!"
+            thre = np.percentile(
+                session_df[f"preds"].values, 100 - self.anomaly_ratio * 100
+            )
+            pred = (session_df[f"preds"] > thre).astype(int)
+        
+            return pred
+
+    def __evaluate_recst(self, test_loader):
+        self.eval()  # set to evaluation mode
+        with torch.no_grad():
+            y_pred = []
+            store_dict = defaultdict(list)
+            infer_start = time.time()
+            for batch_input in test_loader:
+                return_dict = self.forward(self.__input2device(batch_input))
+                y_pred = return_dict["y_pred"]
+                store_dict["label"].extend(
+                    tensor2flatten_arr(batch_input["label"])
+                )
+                store_dict["preds"].extend(tensor2flatten_arr(y_pred))
+            infer_end = time.time()
+            logging.info("Finish inference [{:.2f}s]".format(infer_end - infer_start))
+            self.time_tracker["test"] = infer_end - infer_start
+
+            store_df = pd.DataFrame(store_dict)
+
+            use_cols = ["label", "preds"]
+            session_df = (
+                store_df[use_cols]
+            )
+            assert (
+                self.anomaly_ratio is not None
+            ), "anomaly_ratio should be specified for autoencoder!"
+            thre = np.percentile(
+                session_df[f"preds"].values, 100 - self.anomaly_ratio * 100
+            )
+            pred = (session_df[f"preds"] > thre).astype(int)
+            y = (session_df["label"] > 0).astype(int)
+
+            eval_results = {
+                "f1": f1_score(y, pred),
+                "rc": recall_score(y, pred),
+                "pc": precision_score(y, pred),
+                "acc": accuracy_score(y, pred),
+            }
+            logging.info({k: f"{v:.3f}" for k, v in eval_results.items()})
+            return eval_results
