eabe087b7a0473cd6098fcf2b2758fba19961b25
okestro-mjsong <mj.song@okestro.com>
2022-12-22 10:29:22 +0900

    log-anomaly-detection commit

--- pipelines/preprocessing/preprocessing_pipeline.py
+++ pipelines/preprocessing/preprocessing_pipeline.py
@@ -4,7 +4,7 @@ from kfp import components
 from kfp.components import func_to_container_op
 from elasticsearch import Elasticsearch
 import kubernetes.client
-client = kfp.Client(host='http://34.64.153.235:30020')
+client = kfp.Client(host='ip address')
 
 
 
@@ -13,34 +13,105 @@ def preprocessing(metric : str):
     sys.path.append('/symphony/croffle/pipelines/preprocessing')
 
     if metric == "filesystem":
-        # preprocessing filesystem
         from preprocessing_filesystem import preprocessing_main
         preprocessing_main() 
     elif metric == "cpu":
-        # prerpocessing cpu
         from preprocessing_cpu import preprocessing_main
         preprocessing_main() 
     elif metric == "disk":
-        # preprocessing disk
         from preprocessing_disk import preprocessing_main
         preprocessing_main() 
     elif metric == "memory":
-        # preprocessing memory
         from preprocessing_memory import preprocessing_main
         preprocessing_main() 
     elif metric == "network":
-        # preprocessing network
         from preprocessing_network import preprocessing_main
         preprocessing_main()  
     elif metric == "pod":
-        # preprocessing pod
         from preprocessing_pod import preprocessing_main
         preprocessing_main()   
 
-     
 
-# pipelines components
 preprocessing_cpu_component = components.create_component_from_func(
         func=preprocessing,                 
         base_image='okestroaiops/prediction:latest'
     )
+
+
+
+@dsl.pipeline(
+    name="croffle-preprocessing_pvc",
+    description = "croffle preprocessing pipeline pvc"
+)
+def preprocessing_pipeline(cpu_request :str="4000m", 
+                        cpu_limit : str="8000m",
+                        memory_request : str="4000Mi",
+                        memory_limit : str="16000Mi"):
+    dsl.get_pipeline_conf().set_image_pull_secrets([kubernetes.client.V1LocalObjectReference(name="okestroaiops")])
+    vop = dsl.PipelineVolume(pvc='croffle-pvc')    
+
+    mount_path = "/symphony/"
+
+    preprocessing_task_cpu = preprocessing_cpu_component("cpu").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})
+    preprocessing_task_memory = preprocessing_cpu_component("memory").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(preprocessing_task_cpu)
+    preprocessing_task_disk = preprocessing_cpu_component("disk").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(preprocessing_task_memory)
+    preprocessing_task_filesystem = preprocessing_cpu_component("filesystem").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(preprocessing_task_disk)
+    preprocessing_task_network = preprocessing_cpu_component("network").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(preprocessing_task_filesystem)
+    preprocessing_task_pod = preprocessing_cpu_component("pod").set_cpu_limit(cpu_limit)\
+                                            .set_memory_limit(memory_limit)\
+                                            .set_cpu_request(cpu_request)\
+                                            .set_memory_request(memory_request)\
+                                            .add_pvolumes({mount_path: vop})\
+                                            .after(preprocessing_task_network)
+
+    dsl.get_pipeline_conf().set_ttl_seconds_after_finished(20)
+
+
+client.create_run_from_pipeline_func(preprocessing_pipeline, arguments={})
+
+
+
+kfp.compiler.Compiler().compile(
+    pipeline_func=preprocessing_pipeline,
+    package_path='preprocessing_pl.yaml'
+)
+
+client.create_recurring_run(
+    experiment_id = client.get_experiment(experiment_name="Default").id,
+    job_name="preprocessing_croffle_pvc",
+    description="version: croffle:preprocessing_pvc",
+    cron_expression="0 0 16 * * *",
+    pipeline_package_path = "preprocessing_pl.yaml",
+)
+
+
+
+client.upload_pipeline(
+    pipeline_package_path='preprocessing_pl.yaml',
+    pipeline_name = "preprocessing_croffle_pvc",
+    description = "version: croffle:preprocessing"
+)
